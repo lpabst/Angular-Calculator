92e7437e1f6b6a109465d090cc974adffa4b621a
loren pabst <loren pabst>
2017-06-05 10:17:10 -0600

    most of the functions are working now

--- public/app/routes/home/homeCtrl.js
+++ public/app/routes/home/homeCtrl.js
@@ -3,9 +3,29 @@ angular.module("app")
 
     $scope.display = '0';
 
-    
+    $scope.clear = function(){
+      $scope.display = '0';
+    }
 
-    
+    $scope.backspace = function(){
+      arr = $scope.display.split('');
+      arr.pop();
+      $scope.display = arr.join('');
+    }
+
+    $scope.addToDisplay = function(str){
+      if ($scope.display == '0'){
+        $scope.display = str;
+      }else{
+        $scope.display += str;
+      }
+    }
+
+    $scope.equals = function(){
+      $scope.display = Number($scope.display);
+    }
+
+
 
 
   });
